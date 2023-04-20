f999b6b9f2f05fd51138bd8cdd83539cbb1adb9c
User <lubchenkonata@gmail.com>
2023-04-19 22:26:12 -0400

    day_31_work  Constructor

---
+++ src/day31_custom_classes/Car.java
@@ -0,0 +1,27 @@
+package day31_custom_classes;
+
+public class Car {
+    String model;
+    int year;
+    String color;
+    int fuelLevel;
+
+
+
+    public String toString() {
+        return "Car: " +
+                "\nmodel = " + model +
+                "\nyear= " + year +
+                "\ncolor = " + color +
+                "\nfuelLevel = " + fuelLevel;
+    }
+
+    public void fillTank(){
+        System.out.println("filling thank");
+        fuelLevel = 100;
+    }
+    public void fillTank(int level){
+        System.out.println("filling thank");
+        fuelLevel = fuelLevel+level;
+    }
+}
